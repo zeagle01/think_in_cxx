26d7283b0f3f25aa50fb77e4edc1b6429fd85ee0
zeagle <zeagle01@163.com>
2019-10-28 16:25:54 +0800

    add bulble sort

--- test/intro_2_alg/qsort_test.cpp
+++ test/intro_2_alg/qsort_test.cpp
@@ -30,6 +30,16 @@ namespace QSort {
 		}
 	}
 
+	void bsort(int* A, const int n) {
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n-i-1; j++) {
+				if (A[j] > A[j + 1]) {
+					std::swap(A[j], A[j + 1]);
+				}
+			}
+		}
+	}
+
 }
 
 
@@ -64,6 +74,39 @@ TEST(QSort_Test,test_3212) {
 	std::vector<int> A{ 3,2,1,2 };
 
 	QSort::qsort( &A[0], 0, A.size()-1);
+
+	std::vector<int> exp{ 1,2,2,3 };
+
+	EXPECT_EQ(A, exp);
+}
+
+TEST(BSort_Test,test_321) {
+
+	std::vector<int> A{ 3,2,1 };
+
+	QSort::bsort( &A[0], A.size());
+
+	std::vector<int> exp{ 1,2,3 };
+
+	EXPECT_EQ(A, exp);
+}
+
+TEST(BSort_Test,test_213) {
+
+	std::vector<int> A{ 2,1,3 };
+
+	QSort::bsort( &A[0], A.size());
+
+	std::vector<int> exp{ 1,2,3 };
+
+	EXPECT_EQ(A, exp);
+}
+
+TEST(BSort_Test,test_3212) {
+
+	std::vector<int> A{ 3,2,1,2 };
+
+	QSort::bsort( &A[0], A.size());
 
 	std::vector<int> exp{ 1,2,2,3 };
 
