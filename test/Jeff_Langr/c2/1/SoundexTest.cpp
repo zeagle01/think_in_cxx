df41527c2076352f0a8a1640348300b62d595e6f
zeagle <zeagle01@163.com>
2019-12-14 12:28:48 +0800

    zero pad

--- test/Jeff_Langr/c2/1/SoundexTest.cpp
+++ test/Jeff_Langr/c2/1/SoundexTest.cpp
@@ -4,10 +4,30 @@
 
 class Soundex {
 
+public:
+	std::string encode(const std::string& str) {
+		return zeroPad(str);
+	}
+private:
+	std::string zeroPad(const std::string& str) {
+		return str+"000";
+	}
 };
+
+using namespace  ::testing;
 
 TEST(SoundexEncoding, RetainsSoleLetterOfOneLetterWord) {
 	
 	Soundex soundex;
+	auto encoded=soundex.encode("A");
+	EXPECT_THAT(encoded, Eq("A000"));
 	
+}
+
+
+
+TEST(SoundexEncoding, PadsWithZerosToEnsureThreeDigits) {
+	Soundex soundex;
+	auto encoded = soundex.encode("I");
+	ASSERT_THAT(encoded, Eq("I000"));
 }
