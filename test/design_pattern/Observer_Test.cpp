c6b3ad74baa1c237fd5f5a1a0cbd1306d57d7b69
zeagle <zeagle01@163.com>
2020-04-06 21:21:43 +0800

    observable

--- test/design_pattern/Observer_Test.cpp
+++ test/design_pattern/Observer_Test.cpp
@@ -1,5 +1,6 @@
 
 #include "gmock/gmock.h"
+#include <any>
 
 namespace observer
 {
@@ -8,43 +9,76 @@ namespace observer
     template<typename T>
     class Observer;
 
-    class Person
+
+    template<typename T>
+    class Observable
+    {
+        public:
+        void subscript(Observer<T>* ob)
+        {
+            m_observers.push_back(ob);
+        }
+        void notify(T* source, std::string field_name, std::any new_value) ;
+
+    private:
+        std::vector<Observer<T> *> m_observers;
+    };
+
+    class Person : public Observable<Person>
     {
     public:
-        void subscript(Observer<Person> *ob);
+        void set_age(int age);
+    private:
+        int m_age = 0;
     };
 
     template <typename T>
     class Observer
     {
         public:
-        virtual void change_recieved() = 0;
+        virtual void field_changed(T* source,std::string field_name,std::any new_value) {};
     };
 
     class Person_Observer_Mock:public Observer<Person>
     {
         public:
-        MOCK_METHOD(void,change_recieved,(),(override));
+        MOCK_METHOD(void,field_changed,(Person* source,std::string field_name,std::any new_value),(override));
     };
 
-    void Person::subscript(Observer<Person> *ob)
+
+    void Person::set_age(int age)
     {
-        ob->change_recieved();
+        m_age = age;
+        Observable<Person>::notify(this,"age",age);
     }
+
+    template <typename T>
+    void Observable<T>::notify(T *source, std::string field_name, std::any new_value)
+    {
+        for (auto ob : m_observers)
+        {
+            ob->field_changed(source, field_name, new_value);
+        }
+    }
+
 
 } // namespace observer
 
 using namespace testing;
 using namespace observer;
 
-TEST(Observer_Test, test)
+TEST(Observer_Test, test_set_will_trigger_call)
 {
     Person p;
     Person_Observer_Mock ob0;
     Person_Observer_Mock ob1;
-    EXPECT_CALL(ob0, change_recieved);
-    EXPECT_CALL(ob1, change_recieved);
-
     p.subscript(&ob0);
     p.subscript(&ob1);
+
+    EXPECT_CALL(ob0, field_changed(&p,StrEq("age"),_))
+    .Times(1);
+    EXPECT_CALL(ob1, field_changed(&p,StrEq("age"),_))
+    .Times(1);
+
+    p.set_age(33);
 }
