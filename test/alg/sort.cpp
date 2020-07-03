2c01e08608b6cabb73f862a66136ec7cb6561d4b
zeagle <zeagle01@163.com>
2020-07-03 23:59:39 +0800

    merge sort

--- test/alg/sort.cpp
+++ test/alg/sort.cpp
@@ -3,6 +3,7 @@
 #include "gmock/gmock.h"
 
 #include <vector>
+#include <limits>
 
 namespace sort
 {
@@ -33,18 +34,52 @@ namespace sort
 	public:
 		void apply(std::vector<int>& in)
 		{
-			int q = in.size() / 2;
-			sort_sub(in, 0, q);
-			sort_sub(in, 1, in.size());
-			merge(in, 0, q, in.size());
+			sort_sub(in, 0, in.size());
 		}
 	private:
-		void sort_sub(std::vector<int>& in, int p, int q) 
+		void sort_sub(std::vector<int>& in, int p, int r) 
 		{
-
+			if (r - p > 1)
+			{
+				int q = (p + r) / 2;
+				sort_sub(in, p, q);
+				sort_sub(in, q, r);
+				merge(in, p, q, r);
+			}
 		}
 		void merge(std::vector<int>& in, int p, int q, int r)
 		{
+			std::vector<int> lv(q - p + 1);
+			std::vector<int> rv(r - q + 1);
+			for (int i = 0; i < q - p ; i++)
+			{
+				lv[i] = in[p + i];
+			}
+			lv.back() = std::numeric_limits<int>::max();
+			for (int i = 0; i < r - q; i++)
+			{
+				rv[i] = in[q + i];
+			}
+			rv.back() = std::numeric_limits<int>::max();
+
+
+			int li = 0;
+			int ri = 0;
+			for (int i = p; i < r; i++)
+			{
+				if (lv[li] < rv[ri])
+				{
+
+					in[i] = lv[li];
+					li++;
+				}
+				else
+				{
+					in[i] = rv[ri];
+					ri++;
+				}
+
+			}
 
 		}
 	};
@@ -68,13 +103,24 @@ TEST(Sort_Test, insertion_sort_321)
 
 }
 
-TEST(Sort_Test, insertion_sort_312)
+TEST(Sort_Test, merge_sort_312)
 {
 	std::vector<int> act{ 3,1,2 };
-	Insertion_Sort s;
+	Merge_Sort s;
 	s.apply(act);
 
 	std::vector<int> exp{ 1,2,3 };
+	EXPECT_THAT(act, Eq(exp));
+
+}
+
+TEST(Sort_Test, merge_sort_3412)
+{
+	std::vector<int> act{ 3,4,1,2 };
+	Merge_Sort s;
+	s.apply(act);
+
+	std::vector<int> exp{ 1,2,3 ,4 };
 	EXPECT_THAT(act, Eq(exp));
 
 }
