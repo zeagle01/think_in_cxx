1dc2e52a72de762de1c56044a8b0fccf552d6be0
zeagle <zeagle01@163.com>
2020-10-16 11:05:54 +0800

    non virtual derived class

--- test/lang/Subclass_Visit_Supclass.cpp
+++ test/lang/Subclass_Visit_Supclass.cpp
@@ -2,41 +2,82 @@
 
 #include "gmock/gmock.h"
 
+#include <vector>
 
+
+using namespace testing;
 
 namespace subclass_visit
 {
-    class Base
-    {
-	public:
-        int a;
-        void f()
-        {
+	namespace _1
+	{
+		class Base
+		{
+		public:
+			int a;
+			void f()
+			{
 
-			printf("in base f\n");
-        }
+				printf("in base f\n");
+			}
 
-    };
+		};
 
-    class Derived:public Base
-    {
-	public:
-        int b;
-        void g()
-        {
-			printf("int derived g\n");
-        }
-    };
+		class Derived :public Base
+		{
+		public:
+			int b;
+			void g()
+			{
+				printf("int derived g\n");
+			}
+		};
+
+
+
+		TEST(subclass_visit, use)
+		{
+			Base *a = new Base;;
+			a->f();
+			((Derived*)(a))->g();
+		}
+	}
+
+	namespace _2
+	{
+
+		struct Base
+		{
+			int b;
+		};
+
+		struct Derived:public Base
+		{
+			int d;
+		};
+
+
+
+		TEST(subclass_visit, sub_non_virtual_class_vector)
+		{
+			std::vector<Base> bases;
+
+			bases.push_back(Derived{ 1,2 });
+			bases.push_back(Base{ 0 });
+
+
+			EXPECT_THAT(bases[0].b, Eq(1));
+
+			//EXPECT_THAT(bases[1].d, Eq(2)); //do not compiles
+
+			auto act = ((Derived*)(&bases[1]))->d;
+			EXPECT_THAT(act, Ne(2)); 
+		}
+	}
 
 }
 
-using namespace testing;
-using namespace subclass_visit;
 
 
-TEST(subclass_visit,use)
-{
-    Base *a=new Base;;
-    a->f();
-    ((Derived*)(a))->g();
-}
+
+
