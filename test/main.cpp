eac3ed052766afdcd718e35b3ee35c54cac6cfcc
zeagle <zeagle01@163.com>
2020-12-04 21:14:03 +0800

    introduce glm with conan

--- test/main.cpp
+++ test/main.cpp
@@ -1,9 +1,11 @@
 
 #include "gmock/gmock.h"
 //#include "gtest/gtest.h"
-
+#include <glm/vec3.hpp> // glm::vec3
 
 int main(int argc,char** argv){
+
+    glm::vec3 a;
 
     testing::InitGoogleMock(&argc,argv);
     return RUN_ALL_TESTS();
