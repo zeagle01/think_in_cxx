c762a326b0b37c3e523d7f64bcc063bdd38a24cd
zeagle <zeagle01@163.com>
2020-12-05 19:37:57 +0800

    value polymorhism

---
+++ test/design_pattern/value_polymorphism.cpp
@@ -0,0 +1,84 @@
+
+
+#include "gmock/gmock.h"
+#include <any>
+#include <string>
+
+using namespace testing;
+
+namespace value_polymorphism
+{
+
+
+	template<typename Interface>
+	class Implementation
+	{
+
+	public:
+		template<typename ConcreteType>
+		Implementation(ConcreteType obj):
+			m_storage(obj),
+			getter(
+				[](std::any& a)-> Interface& {
+					return std::any_cast<ConcreteType>(a);
+				}
+
+			)
+		{}
+
+				Interface* operator->()
+				{
+					return &getter(m_storage);
+				}
+
+
+
+	private:
+		std::any m_storage;
+		Interface& (*getter)(std::any&);
+
+	};
+
+
+
+
+
+	class Base 
+	{
+	public:
+		virtual std::string f() = 0;
+	};
+
+	class A : public Base 
+	{
+	public:
+		std::string f() override
+		{
+			return "A";
+		};
+	};
+
+	class B : public Base 
+	{
+	public:
+		std::string f() override
+		{
+			return "B";
+		};
+	};
+
+
+	TEST(value_polymorphism_Test, use)
+	{
+
+		Implementation<Base> base = A{};
+		auto act = base->f();
+		EXPECT_THAT(act, Eq("A"));
+		base=B{};
+		act = base->f();
+		EXPECT_THAT(act, Eq("B"));
+
+	}
+
+
+}
