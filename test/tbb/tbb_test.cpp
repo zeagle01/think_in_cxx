f9d65287de8dcf1149ff5adc7fa831c7e3e8f602
zeagle <zeagle01@163.com>
2022-04-02 21:54:20 +0800

    tbb basic ussage

--- test/tbb/tbb_test.cpp
+++ test/tbb/tbb_test.cpp
@@ -10,23 +10,38 @@ using namespace testing;
 namespace tbb_test
 {
 
-	TEST(TBB_Test, test_parallel_for)
+	TEST(TBB_Test, basic_test_parallel_for_with_range)
 	{
 		int n = 10;
 		std::vector<int > a(n,1);
 		std::vector<int > b(n,1);
-		std::vector<int > act(n);
+		std::vector<int > act(n, 0);
 
 
 		tbb::parallel_for(
-			tbb::blocked_range<int>(0, n),
-			[&a, &b, &act](const tbb::blocked_range<int>& r) {
+			tbb::blocked_range(0, n),
+			[&a, &b, &act](const auto& r) {
 				for (auto i = r.begin(); i != r.end(); i++)
 				{
-					act[i] = a[i] + b[i];
+					act[i] += a[i] + b[i];
 				}
 			}
 		);
+
+		std::vector<int > exp(n,2);
+		EXPECT_THAT(act, Eq(exp));
+	}
+
+	TEST(TBB_Test, basic_test_parallel_for_with_begin_end)
+	{
+		int n = 10;
+		std::vector<int > a(n,1);
+		std::vector<int > b(n,1);
+		std::vector<int > act(n, 0);
+
+		tbb::parallel_for(0, n, [&](auto i) {
+				act[i] += a[i] + b[i];
+			});
 
 		std::vector<int > exp(n,2);
 
@@ -35,7 +50,7 @@ namespace tbb_test
 	}
 
 
-	TEST(TBB_Test, different_task)
+	TEST(TBB_Test, basic_task_group)
 	{
 		tbb::task_group tg;
 
@@ -49,6 +64,7 @@ namespace tbb_test
 		{
 			tg.run(std::bind(computeFn, i));
 		}
+		tg.wait();
 
 		std::vector<int> exp(n);
 		for (int i = 0; i < n; i++)
