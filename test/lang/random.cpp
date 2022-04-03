c0e90df270fb41e4431253a0c348bc8e9a51c430
zeagle <zeagle01@163.com>
2022-04-03 11:51:31 +0800

    reorganize random use case

--- test/lang/random.cpp
+++ test/lang/random.cpp
@@ -4,44 +4,63 @@
 
 #include <random>
 
+using namespace testing;
 
 namespace random_test
 {
-	//can work but not good: for subtle reason
-	int	non_standard_way()
+
+	TEST(RandomTest, default_engine_test)
 	{
-		static std::default_random_engine e;
-		return (e() + 1) % 6;
+		std::default_random_engine re;
+		int n = 10;
+		std::vector<int> act(n);
+		for (int i = 0; i < n; i++)
+		{
+			act[i] = (re() + 1) % 10;
+
+			EXPECT_THAT(act[i], Ge(0));
+			EXPECT_THAT(act[i], Le(n));
+		}
 	}
 
-	int	non_standard_way_with_seed()
+	TEST(RandomTest, generate_int_with_mt_and_device_seed)
 	{
-		static std::default_random_engine e{ 0 };
-		return (e() + 1) % 6;
+		std::random_device rd;//serve as random seed
+		
+		std::mt19937 mt{ rd() }; //random generator
+		std::uniform_int_distribution<int> dist(0, 10); //distribution
+		int n = 10;
+		std::vector<int> act(n);
+		for (int i = 0; i < n; i++)
+		{
+			act[i] = dist(mt);
+
+			EXPECT_THAT(act[i], Ge(0));
+			EXPECT_THAT(act[i], Le(n));
+		}
+	}
+
+	TEST(RandomTest, generate_int_with_seed)
+	{
+		
+		std::default_random_engine re;//random generator
+		re.seed(10);
+		std::uniform_int_distribution<int> dist(0, 10); //distribution
+		int n = 10;
+		std::vector<int> act(n);
+		for (int i = 0; i < n; i++)
+		{
+			act[i] = dist(re);
+
+			EXPECT_THAT(act[i], Ge(0));
+			EXPECT_THAT(act[i], Le(n));
+		}
 	}
 
 }
 
 
 
-using namespace testing;
 
 
-TEST(RandomTest, generate_int)
-{
-	std::random_device rd;//serve as random seed
-	std::default_random_engine re;//random generator
-	re.seed(10);
-	std::mt19937 mt{ rd() }; //random generator
-	std::uniform_int_distribution<int> dist(0, 10); //distribution
-	int n = 10;
-	std::vector<int> act(n);
-	for (int i = 0; i < n; i++)
-	{
-		act[i] = dist(mt);
-
-		EXPECT_THAT(act[i], Ge(0));
-		EXPECT_THAT(act[i], Le(10));
-	}
-}
 
