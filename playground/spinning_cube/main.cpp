cc466ae61e5fbdbc12f3b81cf8a36f5e1493c4be
zeagle <zeagle01@163.com>
2022-09-24 22:24:36 +0800

    ascII shading

--- playground/spinning_cube/main.cpp
+++ playground/spinning_cube/main.cpp
@@ -1,86 +1,107 @@
 
 #include <iostream>
-#include <vector> 
+#include <vector>
 #include <thread>
-#include <algorithm>  
-#include <array>  
 
 
-#define real float
+template<int N,int M>
+struct mat;
 
-template< int N, int M>
+template<int N,int M>
+struct mat_view
+{
+	using fptr = float*;
+	fptr dataRef[N * M];
+
+	float& operator()(int i, int j) { return *dataRef[i * M + j]; }
+	const float& operator()(int i, int j) const { return *dataRef[i * M + j]; }
+
+	void register_ref(int i, int j, float* v_ptr) { dataRef[i * M + j] = v_ptr; }
+
+	operator mat<N, M>() 
+	{
+		mat<N, M> ret;
+		for (int i = 0; i < N * M; i++)
+		{
+			ret.data[i] = *dataRef[i];
+		}
+		return ret;
+	}
+
+	mat_view& operator=(const mat<N, M>& a);
+};
+
+
+template<int N,int M>
 struct mat
 {
-	std::array<real, N* M> data;
+	float data[N * M];
 
-	const real& operator()(int i, int j) const
-	{
-		return data[i * M + j];
-	}
+	float& operator()(int i, int j) { return data[i * M + j]; }
+	const float& operator()(int i, int j) const { return data[i * M + j]; }
 
-	real& operator()(int i, int j)
+	template<int bN,int bM>
+	mat_view<bN, bM> block(int i0, int j0)
 	{
-		return data[i * M + j];
+		mat_view<bN, bM> view;
+		for (int i = 0; i < bN; i++)
+		{
+			for (int j = 0; j < bM; j++)
+			{
+				view.register_ref(i, j, &(operator()(i0 + i, j0 + j)));
+			}
+		}
+		return view;
 	}
 };
 
-template< int N >
-struct mat<N,1>
+template<int N, int M>
+mat_view<N,M>& mat_view<N, M>::operator=(const mat<N, M>& a)
 {
-	std::array<real, N> data;
-
-	const real& operator()(int i, int j) const
+	for (int i = 0; i < N * M; i++)
 	{
-		return data[i * 1 + j];
+		*(dataRef[i]) = a.data[i];
 	}
+	return *this;
+}
 
-	real& operator()(int i, int j)
-	{
-		return data[i * 1 + j];
-	}
 
-	const real& operator()(int i) const
-	{
-		return data[i ];
-	}
+template<int N>
+struct mat<N, 1>
+{
 
-	real& operator()(int i)
-	{
-		return data[i];
-	}
+	float data[N ];
+
+	float& operator()(int i, int j) { return data[i * 1 + j]; }
+	const float& operator()(int i, int j) const { return data[i * 1 + j]; }
+
+	float& operator()(int i) { return data[i]; }
+	const float& operator()(int i) const { return data[i]; }
+
 };
 
-template< int N, int L,int M>
-static mat<N, M> operator*(const mat<N, L> a, const mat<L, M> b)
+
+
+template< int N,int L,int M>
+mat<N, M> operator*(const mat<N, L>& a, const mat<L, M>& b)
 {
 	mat<N, M> ret;
-	for (int ni = 0; ni < N; ni++)
+	for (int i = 0; i < N; i++)
 	{
-		for (int mi = 0; mi < M; mi++)
+		for (int j = 0; j < M; j++)
 		{
-			ret(ni,mi) = 0.f;
-			for (int li = 0; li < L; li++)
+			ret(i, j) = 0;
+			for (int l = 0; l < L; l++)
 			{
-				ret(ni, mi) += a(ni, li) * b(li, mi);
+				ret(i, j) += a(i, l) * b(l, j);
 			}
 		}
 	}
 	return ret;
 }
 
-template<int N,int M>
-static mat<N, M> operator-(const mat<N, M>& a, const mat<N, M>& b)
-{
-	mat<N, M> ret;
-	for (int i = 0; i < N * M; i++)
-	{
-		ret.data[i] = a.data[i] - b.data[i];
-	}
-	return ret;
-}
-
-template<int N,int M>
-static mat<N, M> operator+(const mat<N, M>& a, const mat<N, M>& b)
+template< int N,int M>
+mat<N, M> operator+(const mat<N, M>& a, const mat<N, M>& b)
 {
 	mat<N, M> ret;
 	for (int i = 0; i < N * M; i++)
@@ -90,59 +111,144 @@ static mat<N, M> operator+(const mat<N, M>& a, const mat<N, M>& b)
 	return ret;
 }
 
-template<int N,int M>
-static real length(const mat<N, M>& a)
+template< int N,int M>
+mat<N, M> operator-(const mat<N, M>& a, const mat<N, M>& b)
 {
-	real l = 0;
+	mat<N, M> ret;
 	for (int i = 0; i < N * M; i++)
 	{
-		l += a.data[i] * a.data[i];
-	}
-	return std::sqrt(l);
-}
-
-template<int N>
-static mat<N,1> normalized(const mat<N, 1>& a)
-{
-	real l = length(a);
-	mat<N, 1> ret;
-	for (int i = 0; i < N; i++)
-	{
-		ret.data[i] = a.data[i] / l;
+		ret.data[i] = a.data[i] - b.data[i];
 	}
 	return ret;
 }
 
-static mat<3, 1> cross(const mat<3, 1>& a, const mat<3, 1>& b)
+template< int N,int M>
+mat<N, M> operator-(const mat<N, M>& a)
+{
+	mat<N, M> ret;
+
+	for (int i = 0; i < N * M; i++)
+	{
+		ret.data[i] = -a.data[i];
+	}
+
+	return ret;
+}
+
+mat<3, 1> cross(const mat<3, 1>& a, const mat<3, 1>& b)
 {
 	mat<3, 1> ret;
 	ret(0) = a(1) * b(2) - a(2) * b(1);
-	ret(1) = b(0) * a(2) - b(2) * a(0);
-	ret(2) = a(0) * a(1) - a(1) * b(0);
+	ret(1) = -a(0) * b(2) + a(2) * b(0);
+	ret(2) = a(0) * b(1) - a(1) * b(0);
 	return ret;
 }
 
-
-
-template< int N >
-static mat<N, N> get_identity()
+template<int N,int M> 
+float dot(const mat<N, M>& a, const mat<N, M>& b)
 {
-	mat<N, N> ret;
-	for (int i = 0; i < N * N; i++)
+	float ret = 0.f;
+	for (int i = 0; i < N * M; i++)
 	{
-		ret.data[i] = 0.f;
+		ret += a.data[i] * b.data[i];
 	}
+	return ret;
+}
+
+template<int N,int M>
+float length(const mat<N, M>& a)
+{
+	return std::sqrt(dot(a, a));
+}
+
+template<int N>
+mat<N, 1> normalize(const mat<N, 1>& a)
+{
+	mat<N, 1> ret;
+
+	float l = length(a);
+
 	for (int i = 0; i < N; i++)
 	{
-		ret(i, i) = 1.f;
+		ret(i) = a(i) / l;
 	}
+
 	return ret;
 }
 
-using vec3 = mat<3, 1>;
+
+
+template<int N>
+mat<N, N> get_identity()
+{
+	mat<N, N> ret;
+
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < N; j++)
+		{
+			if (i == j)
+			{
+				ret(i, j) = 1.f;
+			}
+			else
+			{
+				ret(i, j) = 0.f;
+			}
+		}
+	}
+
+	return ret;
+}
+
+
+using mat4 = mat<4, 4>;
 using mat3 = mat<3, 3>;
 using vec4 = mat<4, 1>;
-using mat4 = mat<4, 4>;
+using vec3 = mat<3, 1>;
+
+
+
+
+class Screen_Buffer
+{
+public:
+	void set_size(int width, int height)
+	{
+		this->width = width;
+		this->height = height;
+		data.resize((width + 1) * height);
+
+		for (int j = 0; j < height; j++)
+		{
+			operator()(j, width) = '\n';
+		}
+	}
+
+	void clear(char c)
+	{
+		for (int i = 0; i< width; i++)
+		{
+			for (int j = 0; j < height; j++)
+			{
+				operator()(j, i) = c;
+			}
+		}
+	}
+
+	void flush_out()
+	{
+		fwrite(data.data(), 1, data.size(), stdout);
+	}
+
+	char& operator() (int i, int j) { return data[i * (width + 1) + j]; }
+	const char& operator() (int i, int j) const { return data[i * (width + 1) + j]; }
+
+private:
+	std::vector<char> data;
+	int width;
+	int height;
+};
 
 
 
@@ -151,278 +257,290 @@ class Spinning_Cube
 public:
 	void run()
 	{
-		send_cursor_home_position();
 
-		int step = 0;
+		move_cursor_home();
+
+		buffer.set_size(width, height);
+		depth_buffer.resize(width * height);
+
 		while (true)
 		{
-			buffer.assign(viewport_width * viewport_height, ' ');
-			zbuffer.assign(viewport_width * viewport_height, 0.f);
+			buffer.clear(' ');
+			depth_buffer.assign(width * height, 0.f);
 
-			{
-				delta_fov = std::sin(float(step) * 0.1) * 10;
-				fov = fov0 + delta_fov;
-				step++;
-			}
+			draw_cube();
 
-			for (real cube_x = -cube_width; cube_x < cube_width; cube_x += cube_unit)
-			{
-				for (real cube_y = -cube_width; cube_y < cube_width; cube_y += cube_unit)
-				{
-					calculate_point(cube_x, cube_y, -cube_width, '@'); //back side of the cube
-					calculate_point(cube_x, cube_y, cube_width, '#'); //
-					calculate_point(cube_width, cube_x, cube_y, '$'); //
-					calculate_point(-cube_width, cube_x, cube_y, '~'); //
-					calculate_point(cube_x, -cube_width, cube_y, '+'); //
-					calculate_point(cube_x, cube_width, cube_y, ';'); //
-				}
-			}
+			buffer.flush_out();
 
-			print_buffer();
-
-			A += 0.05;
-			B += 0.05;
-			C += 0.01;
+			//angle_x += 0.005f;
+			//angle_y += 0.005f;
+			//angle_z += 0.001f;
 
 			using namespace std::chrono_literals;
-			std::this_thread::sleep_for(1ms);
+			std::this_thread::sleep_for(5ms);
 		}
-
 	}
 
 private:
-	void send_cursor_home_position()
-	{
-		//printf("\x1b[2J");
-		printf("\x1b[H");
-	}
-
-	void print_buffer()
-	{
-		for (int i = 0; i < viewport_width * viewport_height; i++)
-		{
-			buffer[i] = i % viewport_width ? buffer[i] : '\n';
-		}
-		fwrite(buffer.data(), 1, viewport_width * viewport_height, stdout);
-	}
 
 	mat4 get_rotation_x()
 	{
-		mat4 ret = get_identity<4>();
-		float c = std::cos(A);
-		float s = std::sin(A);
-		ret(1, 1) = c;
-		ret(1, 2) = s;
-		ret(2, 1) = -s;
-		ret(2, 2) = c;
-		return ret;
+		float c = std::cos(angle_x);
+		float s = std::sin(angle_x);
+		mat4 rot =
+		{
+			1.f,0.f,0.f,0.f,
+			0.f,c,s,0.f,
+			0.f,-s,c,0.f,
+			0.f,0.f,0.f,1.f
+		};
+		return rot;
 	}
 
 	mat4 get_rotation_y()
 	{
-		mat4 ret = get_identity<4>();
-		float c = std::cos(B);
-		float s = std::sin(B);
-		ret(0, 0) = c;
-		ret(0, 2) = s;
-		ret(2, 0) = -s;
-		ret(2, 2) = c;
-		return ret;
+		float c = std::cos(angle_y);
+		float s = std::sin(angle_y);
+		mat4 rot =
+		{
+			c,0.f,s,0.f,
+			0.f,1.f,0.f,0.f,
+			-s,0.f,c,0.f,
+			0.f,0.f,0.f,1.f
+		};
+		return rot;
 	}
 
 	mat4 get_rotation_z()
 	{
-		mat4 ret = get_identity<4>();
-		float c = std::cos(B);
-		float s = std::sin(B);
-		ret(0, 0) = c;
-		ret(0, 1) = s;
-		ret(1, 0) = -s;
-		ret(1, 1) = c;
-		return ret;
+		float c = std::cos(angle_z);
+		float s = std::sin(angle_z);
+		mat4 rot =
+		{
+			c,s,0.f,0.f,
+			-s,c,0.f,0.f,
+			0.f,0.f,1.f,0.f,
+			0.f,0.f,0.f,1.f
+		};
+		return rot;
 	}
 
 	mat4 get_view_matrix()
 	{
-		mat4 ret = get_identity<4>();
-		vec3 z = normalized(camara_position - look_at_posiiton);
-		vec3 x = normalized(cross(up, z));
-		vec3 y = normalized(cross(z, x));
-		ret(0, 0) = x(0);
-		ret(1, 0) = x(1);
-		ret(2, 0) = x(2);
+		mat4 rotation = get_identity<4>();
 
-		ret(0, 1) = y(0);
-		ret(1, 1) = y(1);
-		ret(2, 1) = y(2);
 
-		ret(0, 2) = z(0);
-		ret(1, 2) = z(1);
-		ret(2, 2) = z(2);
+		auto z = normalize(camara_pos - look_at);
+		auto x = normalize(cross(up, z));
+		auto y = normalize(cross(z, x));
 
-		float perspective_factor = 1.f;
-
-		ret(2, 0) *= perspective_factor;
-		ret(2, 1) *= perspective_factor;
-		ret(2, 2) *= perspective_factor;
+		rotation.block<3, 1>(0, 0) = x;
+		rotation.block<3, 1>(0, 1) = y;
+		rotation.block<3, 1>(0, 2) = z;
 
 		mat4 inverse_translate = get_identity<4>();
-		inverse_translate(0, 3) = -camara_position(0);
-		inverse_translate(1, 3) = -camara_position(1);
-		inverse_translate(2, 3) = -camara_position(2);
+		inverse_translate.block<3, 1>(0, 3) = -camara_pos;
 
+		return  rotation * inverse_translate;
+	}
 
-		ret = ret * inverse_translate;
-		return ret ;
+	float degree_2_radian(float d)
+	{
+		const float pi = 3.1415926f;
+		return d * pi / 180.f;
+	}
+
+	mat4 get_viewport_matrix()
+	{
+		mat4 ret = get_identity<4>();
+
+		float t = std::tan(degree_2_radian(fov) / 2.f);
+		float half_space_height = std::abs(view_near) * t;
+		float half_space_width = half_space_height * aspect;
+
+		vec3 x_low_from{ -half_space_width,-half_space_height,view_near };
+		vec3 x_high_from{ half_space_width,half_space_height,view_far };
+
+		vec3 x_low_to{ 0,0,view_near };
+		vec3 x_high_to{ width,height,view_far };
+
+		mat4 move_back_to_origin = get_identity<4>();
+		move_back_to_origin.block<3, 1>(0, 3) = -x_low_from;
+
+		mat4 scale = get_identity<4>();
+		for (int i = 0; i < 3; i++)
+		{
+			scale(i, i) = (x_high_to(i) - x_low_to(i)) / (x_high_from(i) - x_low_from(i));
+		}
+
+		mat4 move_to_to = get_identity<4>();
+		move_to_to.block<3, 1>(0, 3) = x_low_to;
+
+		return move_to_to * scale * move_back_to_origin;
 	}
 
 	mat4 get_perspective_matrix()
 	{
 		mat4 ret = get_identity<4>();
+
 		ret(0, 0) = view_near;
 		ret(1, 1) = view_near;
 		ret(2, 2) = view_near + view_far;
 		ret(3, 3) = 0.f;
-
-		ret(2, 3) = -view_near * view_far;
 		ret(3, 2) = 1.f;
+		ret(2, 3) = -view_near * view_far;
+
 		return ret;
 	}
 
 
-	mat4 compute_view_volumn_transform_matrix(const vec3& x0_from, const vec3& x1_from, const vec3& x0_to, const vec3& x1_to)
+	void draw_pixel(const vec3& x, const vec3& normal, char c)
 	{
-		mat4 inverse_t0 = get_identity<4>();
-		inverse_t0(0, 3) = -x0_from(0);
-		inverse_t0(1, 3) = -x0_from(1);
-		inverse_t0(2, 3) = -x0_from(2);
+		vec4 pos{ x(0),x(1),x(2),1.0f };
 
-		mat4 scale = get_identity<4>();
-		scale(0, 0) = (x1_to(0) - x0_to(0)) / (x1_from(0) - x0_from(0));
-		scale(1, 1) = (x1_to(1) - x0_to(1)) / (x1_from(1) - x0_from(1));
-		scale(2, 2) = (x1_to(2) - x0_to(2)) / (x1_from(2) - x0_from(2));
-
-		mat4 t1 = get_identity<4>();
-
-		t1(0, 3) = x0_to(0);
-		t1(1, 3) = x0_to(1);
-		t1(2, 3) = x0_to(2);
-
-		return t1 * scale * inverse_t0;
-
-	}
-
-	float degree_2_radian(float degree)
-	{
-		const float pi = 3.1415926;
-		return  degree * pi / 180.f;
-	}
-
-	mat4 get_viewport_matrix()
-	{
-		vec3 x0_from{ -1,-1,-1 };
-		vec3 x1_from{ 1,1,1 };
-
-		vec3 x0_to{ 0,0, view_near };
-		vec3 x1_to{ viewport_width,viewport_height,view_far };
-
-		return compute_view_volumn_transform_matrix(x0_from, x1_from, x0_to, x1_to);
-
-	}
-
-	mat4 get_view_volumn_matrix()
-	{
-		float half_space_height = std::abs(view_near) * std::tan(degree_2_radian(fov / 2.f));
-		float half_space_width = half_space_height * aspect;
-
-		vec3 x0_from{ -half_space_width,-half_space_height,view_near };
-		vec3 x1_from{ half_space_width,half_space_height,view_far };
-
-		vec3 x0_to{ -1,-1,-1 };
-		vec3 x1_to{ 1,1,1 };
-
-		return compute_view_volumn_transform_matrix(x0_from, x1_from, x0_to, x1_to);
-	}
-
-	void calculate_point(real cubeX, real cubeY, real cubeZ, char ch)
-	{
-
-		auto rx = get_rotation_x();
-		auto ry = get_rotation_y();
-		auto rz = get_rotation_z();
-
-		vec4 pos{ cubeX,cubeY,cubeZ,1.f };
+		auto rot_x = get_rotation_x();
+		auto rot_y = get_rotation_y();
+		auto rot_z = get_rotation_z();
+		pos = rot_z * rot_y * rot_x * pos;
 
 		auto view_matrix = get_view_matrix();
+		pos = view_matrix * pos;
+		auto view_pos = pos;
 
 		auto perspective_matrix = get_perspective_matrix();
-		auto view_volumn_matrix = get_view_volumn_matrix();
+		pos = perspective_matrix * pos;
+
 		auto viewport_matrix = get_viewport_matrix();
+		pos = viewport_matrix * pos;
 
-		vec4 p0 = view_matrix * (rz * ry * rx) * pos;
-		vec4 p1 = view_volumn_matrix * perspective_matrix * p0;
-		vec4 p2 = viewport_matrix * p1;
-		vec4 pos_screen =  p2;
+		vec3 sx{ pos(0) / pos(3), pos(1) / pos(3), pos(2) / pos(3) };
 
-		float x = pos_screen(0) / pos_screen(3);
-		float y = pos_screen(1) / pos_screen(3);
-		float z = pos_screen(2) / pos_screen(3);
-		float ooz = 1.f / z;
+		int xi = int(sx(0));
+		int yi = int(sx(1));
 
 
-		int x_screen = (int)(x);
-		int y_screen = (int)(y);
-
-		int idx = x_screen + y_screen * viewport_width;
-
-		if (idx >= 0 && idx < viewport_width * viewport_height) //on screen range
+		if (xi >= 0 && xi < width && yi >= 0 && yi < height)
 		{
-			if (std::abs(ooz) > std::abs(zbuffer[idx]))// depth test only keep the nearest pixel
+			float depth = 1.f / std::abs(sx(2));
+			int idx = yi * width + xi;
+			if (depth > depth_buffer[idx])
 			{
+				auto T = view_matrix * rot_z * rot_y * rot_x;
 
-				zbuffer[idx] = std::abs(ooz);
-				buffer[idx] = ch;
+				auto n_homo = T * vec4{ normal(0), normal(1), normal(2), 0.f };
+				auto p_homo = T * vec4{ x(0),x(1),x(2),1.0f };
+				auto l_homo = view_matrix * vec4{ light_position(0), light_position(1), light_position(2), 1.f };
+
+				auto l = vec3{ l_homo(0), l_homo(1), l_homo(2) };
+				auto p = vec3{ p_homo(0), p_homo(1), p_homo(2) };
+				auto n = vec3{ n_homo(0), n_homo(1), n_homo(2) };
+				auto e = normalize(-p);
+				//if (n(2) < 0.f)
+				//{
+				//	printf("here\n");
+				//}
+
+				auto l_dir = normalize(l - p);
+				float diffuse = std::max(dot(n, l_dir), 0.f);
+
+				auto h = normalize((l_dir + e));
+
+				float specular = std::max(std::pow(dot(h, n), 2e3f), 0.f);
+
+				//if (xi == width / 2 && yi == height / 2)
+				//{
+				//	printf(" %f %f %f, %f %f %f, %f %f %f, %f %f %f, %f\n", x(0), x(1), x(2), n(0), n(1), n(2), l_dir(0), l_dir(1), l_dir(2), e(0), e(1), e(2), specular);
+				//}
+
+				float density = std::clamp(0.6f * diffuse + 0.4f * specular, 0.00f, 0.99f);
+				//float density = std::clamp(diffuse, 0.00f, 0.99f);
+				//float density = std::clamp(specular, 0.00f, 0.99f);
+
+				depth_buffer[idx] = depth;
+
+				auto density_char = get_density_char(density);
+				//buffer(yi, xi) = density_char;
+				if (density_char == '@')
+				{
+					buffer(yi, xi) = c;
+				}
+				else
+				{
+					buffer(yi, xi) = density_char;
+				}
 			}
 		}
 
 	}
 
+	void draw_cube()
+	{
+		for (float i = -cube_width / 2; i < cube_width / 2; i += cube_unit)
+		{
+			for (float j = -cube_width / 2; j < cube_width / 2; j += cube_unit)
+			{
+				draw_pixel({ i, j, cube_width / 2 }, { 0, 0, 1 },'F');
+				draw_pixel({ i, j, -cube_width / 2 }, { 0, 0, -1 },'B');
+				draw_pixel({ i, -cube_width / 2, j }, { 0,-1,0 },'D');
+				draw_pixel({ i, cube_width / 2, j }, { 0,1,0 },'U');
+				draw_pixel({ -cube_width / 2, i, j }, { -1,0,0 },'L');
+				draw_pixel({ cube_width / 2, i, j }, { 1,0,0 }, 'R');
+			}
+		}
+	}
 
-private:
-	std::vector<char > buffer;
-	std::vector<real > zbuffer;
-	int viewport_width = 60;
-	int viewport_height = 30;
+	void move_cursor_home()
+	{
+		printf("\x1b[H");
+	}
 
-	vec3 camara_position{ 0.f,0.f,80.f };
-	vec3 up{ 0.f,1.f,0.f };
-	vec3 look_at_posiiton{ 0.f,0.f,0.f };
+	static char get_density_char(float d)
+	{
+		static const char density[] = { '@','#','W','$','9','8','6','5','4','3','2','7','0','?','a','b','c','1','!',';',':','+','=',',','.','_' };
 
-	real view_near		= -0.1f;
-	real view_far		= -1e3f;
+		int size = sizeof(density);
+		int p = (int)size * d;
+		return density[p];
+	}
 
-	real fov = 50.f;
-	real pixel_aspect = 0.5f;// not a square pixel
-	real aspect = pixel_aspect * viewport_width / viewport_height;
+	int width = 60;
+	int height = 30;
+	Screen_Buffer buffer;
+	std::vector<float> depth_buffer;
 
+	float cube_width = 20;
+	float cube_unit = 0.5;
 
-	real cube_width = 20.f;
-	real cube_unit = 0.6f;
+	vec3 light_position{ 0,5,50 };
 
-	real A = 0.f;
-	real B = 0.f;
-	real C = 0.f;
+	vec3 up{ 0, 1, 0 };
+	vec3 camara_pos{ 0, 0, 100 };
+	vec3 look_at{ 0, 0, 0 };
 
-	real fov0 = 60;
-	real delta_fov = 0.f;
+	float view_near = -0.01f;
+	float view_far = -1e3f;
+	float fov = 20.f;
+
+	float pixel_aspect = 0.5f;
+	float aspect = pixel_aspect * width / height;
+
+	float angle_x = 0.f;
+	float angle_y = 0.f;
+	float angle_z = 0.f;
 
 };
 
+
 int main()
 {
-	Spinning_Cube sc;
-	sc.run();
+	Spinning_Cube cube;
+
+	cube.run();
+
 	return 0;
 }
+
+
+
 
 
