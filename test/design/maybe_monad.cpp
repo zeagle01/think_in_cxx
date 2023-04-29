0914e3b788d51ca9b29b04342afb738e86d32611
zeagle <zeagle01@163.com>
2023-04-29 13:58:52 +0800

    no owning pointer safe invoke

--- test/design/maybe_monad.cpp
+++ test/design/maybe_monad.cpp
@@ -94,4 +94,68 @@ namespace maybe_monad
 		EXPECT_THAT(act, Eq(exp));
 	}
 
+
+	namespace no_owning_ptr
+	{
+
+		template<typename T>
+		class NoOwningPtr
+		{
+		public:
+			NoOwningPtr(std::shared_ptr<T> obj) :m_data(obj) {};
+			template<typename Fn>
+			decltype(auto) invoke(Fn fn)
+			{
+				if (auto tmp = m_data.lock())
+				{
+					return fn(tmp);
+				}
+				else
+				{
+					using retT = decltype(fn(std::declval<std::shared_ptr<T>>()));
+					return retT{};
+				}
+			}
+
+		private:
+			std::weak_ptr<T> m_data;
+		};
+
+		struct A 
+		{
+			std::string f() {
+				return "A";
+			}
+		};
+
+#define SAFE_INVOKE(f) invoke([](auto a) {return a->f(); })
+
+		TEST(NoOwningPtr, invoke_empty_obj)
+		{
+			std::shared_ptr<A> ptr;
+			NoOwningPtr w_ptr(ptr);
+
+			std::string exp = "";
+
+			std::string act = w_ptr.SAFE_INVOKE(f);
+			EXPECT_THAT(act, Eq(exp));
+
+		}
+
+		TEST(NoOwningPtr, invoke_non_empty_obj)
+		{
+			std::shared_ptr<A> ptr = std::make_shared<A>();
+			NoOwningPtr w_ptr(ptr);
+
+			std::string exp = "A";
+
+			std::string act = w_ptr.SAFE_INVOKE(f);
+
+			EXPECT_THAT(act, Eq(exp));
+
+		}
+
+	}
+
+
 }
