acba2427d0a5497f1cd28964f552eb784ce2844b
zeagle <zeagle01@163.com>
2023-05-07 15:11:57 +0800

    quick sort with tdd

--- test/alg/sort.cpp
+++ test/alg/sort.cpp
@@ -5,6 +5,8 @@
 #include <vector>
 #include <limits>
 
+using namespace testing;
+
 namespace sort
 {
 
@@ -37,7 +39,7 @@ namespace sort
 			sort_sub(in, 0, in.size());
 		}
 	private:
-		void sort_sub(std::vector<int>& in, int p, int r) 
+		void sort_sub(std::vector<int>& in, int p, int r)
 		{
 			if (r - p > 1)
 			{
@@ -51,7 +53,7 @@ namespace sort
 		{
 			std::vector<int> lv(q - p + 1);
 			std::vector<int> rv(r - q + 1);
-			for (int i = 0; i < q - p ; i++)
+			for (int i = 0; i < q - p; i++)
 			{
 				lv[i] = in[p + i];
 			}
@@ -85,42 +87,107 @@ namespace sort
 	};
 
 
+	TEST(Sort_Test, insertion_sort_321)
+	{
+		std::vector<int> act{ 3,2,1 };
+		Insertion_Sort s;
+		s.apply(act);
+
+		std::vector<int> exp{ 1,2,3 };
+		EXPECT_THAT(act, Eq(exp));
+
+	}
+
+	TEST(Sort_Test, merge_sort_312)
+	{
+		std::vector<int> act{ 3,1,2 };
+		Merge_Sort s;
+		s.apply(act);
+
+		std::vector<int> exp{ 1,2,3 };
+		EXPECT_THAT(act, Eq(exp));
+
+	}
+
+	TEST(Sort_Test, merge_sort_3412)
+	{
+		std::vector<int> act{ 3,4,1,2 };
+		Merge_Sort s;
+		s.apply(act);
+
+		std::vector<int> exp{ 1,2,3 ,4 };
+		EXPECT_THAT(act, Eq(exp));
+
+	}
+
+	namespace quick_sort
+	{
+
+
+		class Sorter
+		{
+		public:
+			void sort(std::vector<int>& in)
+			{
+				sort_imp(in.data(), 0, in.size() - 1);
+			}
+
+		private:
+			int find_split(int* d, int b, int e)
+			{
+				int split = b;
+				for (int i = b; i <= e; i++)
+				{
+					if (d[i] <= d[e])
+					{
+						std::swap(d[i], d[split]);
+						split++;
+					}
+					else
+					{
+						continue;
+					}
+				}
+
+				return split - 1;
+			}
+
+			void sort_imp(int* d, int b, int e)
+			{
+				if (b < e)
+				{
+					int split = find_split(d, b, e);
+					sort_imp(d, b, split - 1);
+					sort_imp(d, split + 1, e);
+				}
+			}
+		};
+
+		void test_helper(const std::vector<int>& in, const std::vector<int>& exp)
+		{
+			std::vector<int> act=in;
+			Sorter sorter;
+			sorter.sort(act);
+			EXPECT_THAT(act, Eq(exp));
+		}
+
+		TEST(Quick_Sort, recieve_vector)
+		{
+			test_helper({ 3,2,1 }, { 1,2,3 });
+		}
+
+		TEST(Quick_Sort, recieve_vector_2)
+		{
+			test_helper({ 4,2,1 ,3 }, { 1,2,3,4 });
+		}
+
+		TEST(Quick_Sort, recieve_vector_3)
+		{
+			test_helper({ 4,2,1, 5,3 }, { 1,2,3,4,5 });
+		}
+
+	}
 }
 
 
 
-using namespace testing;
-using namespace sort;
-
-TEST(Sort_Test, insertion_sort_321)
-{
-	std::vector<int> act{ 3,2,1 };
-	Insertion_Sort s;
-	s.apply(act);
-
-	std::vector<int> exp{ 1,2,3 };
-	EXPECT_THAT(act, Eq(exp));
-
-}
-
-TEST(Sort_Test, merge_sort_312)
-{
-	std::vector<int> act{ 3,1,2 };
-	Merge_Sort s;
-	s.apply(act);
-
-	std::vector<int> exp{ 1,2,3 };
-	EXPECT_THAT(act, Eq(exp));
-
-}
-
-TEST(Sort_Test, merge_sort_3412)
-{
-	std::vector<int> act{ 3,4,1,2 };
-	Merge_Sort s;
-	s.apply(act);
-
-	std::vector<int> exp{ 1,2,3 ,4 };
-	EXPECT_THAT(act, Eq(exp));
-
-}
