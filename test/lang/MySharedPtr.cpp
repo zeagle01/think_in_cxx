0683f21fd799dae1315a11da38490fe7a5b421dc
zeagle <zeagle01@163.com>
2023-05-14 00:16:41 +0800

    convertable to bool

--- test/lang/MySharedPtr.cpp
+++ test/lang/MySharedPtr.cpp
@@ -9,10 +9,19 @@ namespace share_ptr_test
 	class Shared_ptr
 	{
 
+	public:
+
+		operator bool()
+		{
+			return false;
+		}
+
 	};
 
 	TEST(Share_Ptr_Test, defaul_constructed_one)
 	{
 		Shared_ptr<int> p;
+
+		EXPECT_FALSE(p);
 	}
 }
