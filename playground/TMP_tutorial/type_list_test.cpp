32a0ded70f13955dc227567ae2e08552b34232c6
zeagle <zeagle01@163.com>
2023-06-11 22:38:30 +0800

    push front to type list

--- playground/TMP_tutorial/type_list_test.cpp
+++ playground/TMP_tutorial/type_list_test.cpp
@@ -32,6 +32,19 @@ template<typename Type_List>
 using Pop_Front = typename Pop_Front_Imp<Type_List>::type;
 
 
+template<typename Type, typename Type_List>
+struct Push_Front_Imp;
+
+template<typename Type,typename ...Tails>
+struct Push_Front_Imp<Type, Type_List< Tails...>>
+{
+	using type = Type_List<Type, Tails...>;
+};
+
+template<typename Type,typename Type_List>
+using Push_Front = Push_Front_Imp<Type, Type_List>::type;
+
+
 
 TEST(Type_List_Test, get_front)
 {
@@ -44,6 +57,13 @@ TEST(Type_List_Test, get_front)
 TEST(Type_List_Test, pop_type_from_list)
 {
 	bool is_type_list_same = std::is_same_v<Pop_Front<Type_List<float, int>>, Type_List< int>>;
+	EXPECT_TRUE(is_type_list_same);
+
+}
+
+TEST(Type_List_Test, push_front_to_type_list)
+{
+	bool is_type_list_same = std::is_same_v<Push_Front<float, Type_List< int>>, Type_List<float, int>>;
 	EXPECT_TRUE(is_type_list_same);
 
 }
